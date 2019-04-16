fe8b327b091d3f79b05fc9024310db0e0ff37e93
Laboratoria-183 <you@example.com>
2019-04-16 12:38:55 -0500

    Subiendo test y funciones

--- index.js
+++ index.js
@@ -1,3 +1,115 @@
-module.exports = () => {
-  // ...
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+
+  //Funcion para verificar si el campo está vacío o lleno
+  pathInserted: function (pathFile) {
+    if (pathFile == undefined) {
+      console.log("false");
+      return false
+    }
+    else {
+      return true
+    }
+  },
+
+  // Función para saber si la ruta existe o no 
+  pathWorking: function (pathFile) {
+    if (fs.existsSync(pathFile)) {
+      console.log("true");
+      return true
+    }
+    else {
+      console.log("false");
+      return false
+    }
+  },
+
+  // function to verificated that path is absolute
+  // pathIsAbsolute : function (pathfile){
+  //   if(path.isAbsolute(pathfile)){
+  //     return true;
+  //   }
+  //   else{
+  //     return false
+  //   }
+  // },
+
+  //Función para verificar si la ruta es un directorio
+
+  pathDirectory: function (pathFile) {
+    if (fs.statSync(pathFile).isDirectory()) {
+      return true
+    } else {
+      return false
+    }
+  },
+
+
+
+  //Funcion que checa si es un .md
+  pathMd: function (pathFile) {
+    if (path.extname(pathFile) === ".md") {
+      return true
+    }
+    else {
+      return false
+    }
+  },
+
+
+  //Funcion para verificar si el campo está vacío o lleno
+  pathInserted: function (pathFile) {
+    if (pathFile == undefined) {
+      console.log("false");
+      return false
+    }
+    else {
+      return true
+    }
+  },
+
+  // Función para saber si la ruta existe o no 
+
+  pathWorking: function (pathFile) {
+    if (fs.existsSync(pathFile)) {
+      console.log("true");
+      return true
+    } else {
+      console.log("false");
+      return false
+    }
+  },
+
+  //Función para verificar si la ruta es un directorio
+
+  pathDirectory: function (pathFile) {
+    if (fs.statSync(pathFile).isDirectory()) {
+      return true
+    } else {
+      return false
+    }
+  },
+
+  //Funcion que checa si es un .md
+  pathMd: function (pathFile) {
+    if (path.extname(pathFile) === ".md") {
+      return true
+    } else {
+      return false
+    }
+  },
+
+  readingFile: function (pathFile) {
+    let archivo = fs.readFileSync(pathFile, 'utf-8');
+    console.log(archivo);
+    return true;
+  },
+
 };
+
+
+
+
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,57 @@
-const mdLinks = require('../');
+const index = require("../index.js");
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe("pathInserted", () => {
+  it("Should return false", () => {
+    expect(index.pathInserted()).toBe(false);
   });
-
+  it("Should return true", () => {
+    expect(index.pathInserted("./README.md")).toBe(true);
+  });
 });
+
+describe("pathWorking", () => {
+  it("Should be false", () => {
+    expect(index.pathWorking("./README.txt")).toBe(false);
+  });
+  it("Should be true", () => {
+    expect(index.pathWorking("./README.md")).toBe(true);
+  });
+});
+
+// describe ('pathIsAbsolute', () => {
+//   it('should be true if user enter a path absolute', () => {
+//     expect(mdLinks.pathIsAbsolute("/home/laboratoria173/Laboratoria/GDL002-md-links/README.md"))
+//       .toBe(true);
+//   })
+//   it('should be false if user enter a NOT path absolute', () => {
+//     expect(mdLinks.pathIsAbsolute("Laboratoria/GDL002-md-links/README.md")).toBe(false);
+//   })
+// })
+
+describe("pathDirectory", () => {
+  it("Should be true", () => {
+    expect(index.pathDirectory("/home/laboratoria-183/Escritorio/ventura")).toBe(true);
+  });
+  it("Should be false", () => {
+    expect(index.pathDirectory("./README.md")).toBe(false);
+  });
+});
+
+describe("pathMd", () => {
+  it("Should be true", () => {
+    expect(index.pathMd("./README.md")).toBe(true);
+  });
+  it("Should be false", () => {
+    expect(index.pathMd("./README.txt")).toBe(false);
+  });
+});
+
+describe("redingFile", () => {
+  it("Should read the file", () => {
+    expect(index.readingFile("./prueba.md")).toBe(true);
+  });
+});
+
+
+
+
